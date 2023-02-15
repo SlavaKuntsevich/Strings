dba78d881b9a36a6fb682528a75e4710187e2f7a
SlavaKuntsevich <mactep_chif@mail.ru>
2023-02-15 23:02:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // TASK 2
+            //string fewWords = "just waking up in the morning gonna thank god";
+            //string[] arrayWords = fewWords.Split(' ');
+            //int indMax = 0, indMin = 0;
+            //int maxWord = arrayWords[indMax].Length, minWord = arrayWords[indMin].Length;
+            //for (int i = 0; i < arrayWords.Length; i++)
+            //{   if (maxWord <= arrayWords[i].Length)
+            //    {
+            //        maxWord = arrayWords[i].Length;
+            //        indMax = i;
+            //    }
+            //    if (minWord >= arrayWords[i].Length)
+            //    {
+            //        minWord = arrayWords[i].Length;
+            //        indMin = i;
+            //    }
+            //}
+            //Console.WriteLine("Минимальное количество символов в слове: " + arrayWords[indMin]);
+            //Console.WriteLine("Максимальное количество символов в слове: " + arrayWords[indMax]);
+            //TASK3
+            //string randomWords = "bicycle,beer,work,gorillaz";
+            //string[] arrayWords = randomWords.Split(',');
+            //string resWords = arrayWords[0];
+            //for (int i = 0; i < arrayWords.Length; i++)
+            //{
+            //    Console.WriteLine(arrayWords[i].Distinct().Count());
+            //    Console.WriteLine(resWords.Distinct().Count());
+            //    Console.WriteLine("\n");
+            //    if (arrayWords[i].Distinct().Count() < resWords.Distinct().Count()) resWords = arrayWords[i] ;
+
+            //}
+            //Console.WriteLine(resWords);
+            // TASK5
+            //Console.Write("Введите текст: ");
+            //string wordS = Console.ReadLine();
+            //string twinWords;
+            //for (int i = 0; i < wordS.Length; i++)
+            //{                
+            //    twinWords = string.Concat(wordS[i], wordS[i]);
+            //    Console.Write(twinWords);
+            //}
+            
+        }
+
+
+    }
+}
